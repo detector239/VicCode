b17bf19dca1870294772aa22777f7186d7947a85
detector239 <detector.239@gmail.com>
2024-10-05 13:34:07 +0300

    CREATE_TABLE.sql file is modified

--- Python/SQL/JOINS/CREATE_TABLE.sql
+++ Python/SQL/JOINS/CREATE_TABLE.sql
@@ -1,4 +1,9 @@
-CREATE TABLE open_archived AS
+-- CREATE TABLE open_archived AS 
+-- SELECT * FROM orders
+-- WHERE order_date < '2019-01-01'
+--  We write this code if the table doesn't exist (creatiing it)
+-- NOTE: the table needs to be deleted (droped) so this code can run
+
+INSERT INTO open_archived 
 SELECT * FROM orders
 WHERE order_date < '2019-01-01'
--- NOTE: the table needs to be deleted (droped) so this code can run
