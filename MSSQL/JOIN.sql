f81cb7e7799b03f25a2fb6bc07f43d0f4e2a6cc3
detector239 <detector.239@gmail.com>
2024-10-18 16:51:52 +0300

    MSSQL INNER JOIN file modified

--- MSSQL/JOIN.sql
+++ MSSQL/JOIN.sql
@@ -3,5 +3,6 @@ OrderDate,
 OrderTotal,
 CustomerName,
 Phone
-FROM KCC.dbo.Orders AS Orders
-Join KCC.dbo.Customers AS Customers ON Orders.CustomerID = Customers.CustomerID
+FROM KCC.dbo.Orders Orders -- <<Orders>> is an alias for <<KCC.dbo.Orders>>
+Join KCC.dbo.Customers Customers ON Orders.CustomerID = Customers.CustomerID
+-- by defoult is INNER JOIN
