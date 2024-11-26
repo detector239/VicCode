bac3ec96118f7d158849afdc0684a8221a4b3bc3
detector239 <detector.239@gmail.com>
2024-11-26 19:52:36 +0200

    Java OOP traning done

--- Java/tutorial/OOP/src/OOP.java
+++ Java/tutorial/OOP/src/OOP.java
@@ -1,13 +1,31 @@
+import java.util.ArrayList;
+
 public class OOP {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        User u = new User();
-        u.name = "Victor";
-        u.membership = "Gold";
+        User u = new User("Victor", "Gold");
+        User u2 = new User("Dan", "Gold");
+        ArrayList<User> users = new ArrayList<User>();
+        users.add(new User("Tom", "Silver"));
 
-        User u2 = new User();
-        u2.name = "Caleb";
-        u2.membership = "Platinum";
-        System.err.println(u.name + " " + u2.membership);
+        User.admins = new ArrayList<User>();
+        User.admins.add(u);
+        User.admins.add(u2);
+
+        for (int i=0; i < User.admins.size(); i++){
+            System.out.println(User.admins.get(i).get_name());
+        }
+//        This is another method of writing this code
+        for (User user : users) {
+            System.out.println(user.get_name());
+        }
+
+        Student s = new Student();
+        s.set_verified(true);
+        System.out.println(s.get_verified());
+        s.set_name("Gabriel");
+        System.out.println(s.get_name());
+
+        User.print_admins();
+
     }
 }
--- Java/tutorial/OOP/src/User.java
+++ Java/tutorial/OOP/src/User.java
@@ -1,3 +1,56 @@
+import java.util.List;
+
+// if we write "public abstract class User" then User() will become a framework for student
+// that means that we no longer be able to use User() as a normal class
 public class User {
-    String name, membership;
+    private String _name;
+    private String _membership = "Bronze";
+
+    public static List<User> admins;
+
+    public static void print_admins(){
+        for(User user : admins) {
+            System.out.println(user.get_name());
+        }
+    }
+
+    public User(){ // this is so we can create a new User without being forced to pass in the name and the membership
+    }
+    public User(String name, String membership) {
+        set_name(name);
+        set_membership(membership);
+    }
+
+    public String toString(){
+        return get_name() + " " + get_membership();
+    }
+
+    public boolean equals(User u){
+        return (get_name() == u.get_name() && get_membership() == u.get_membership());
+    }
+
+
+    void set_name (String name){
+        _name = name;
+    }
+
+    String get_name(){
+        return _name;
+    }
+    void set_membership (String membership){
+        _membership = membership;
+    }
+
+    void set_membership (Membership membership){
+        _membership = membership.name();
+    }
+
+    String get_membership(){
+        return _membership;
+    }
+
+    public enum Membership {
+        Bronze, Silver, Gold;
+    }
+
 }
